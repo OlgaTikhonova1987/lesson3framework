f0fc428cca6109f0bfb87d3150ea226991dcb071
OlgaTikhonova1987 <119539216+OlgaTikhonova1987@users.noreply.github.com>
2022-12-27 01:56:55 +0300

    Ex14

---
+++ src/test/java/lib/Assertions.java
@@ -0,0 +1,13 @@
+package lib;
+
+import io.restassured.response.Response;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.hasKey;
+
+public class Assertions {
+    public static void  asserJsonByName(Response Response, String name, int expectedValue) {
+        Response.then().assertThat().body("$", hasKey(name));
+        int value = Response.jsonPath().getInt(name);
+        assertEquals(expectedValue, value, "JSON value is not equal to expected value");
+    }
+}
